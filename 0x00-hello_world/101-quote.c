071474153f4719dae0020aa311aebd59d82e9a8f
Chifu254 <kyalobrian253@gmail.com>
2023-03-16 21:39:54 +0000

    up

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main-a program that prints
+ *
+ * Return: one
+ *
+ */
+int main(void)
+{
+fprint(stdre, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
