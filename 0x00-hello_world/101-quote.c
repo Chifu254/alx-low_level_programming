30b46a89f214f136792c7db334c34316f0d226ce
Chifu254 <kyalobrian253@gmail.com>
2023-03-17 08:32:22 +0000

    news

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,17 @@
-#include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - prints to the standard error
- * Return: one
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
-fprintf(stdout, "and that piece of art is useful\"- Dora Korpar,2015-10-19\n");
-	return (1);
+ssize_t n;
+n = write(STDERR_FILENO,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+59);
+if (n == -1)
+return (1);
+return (0);
 }
