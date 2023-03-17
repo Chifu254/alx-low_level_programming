7e149cbd7891033fbc297cb1c9689383614d35c4
Chifu254 <kyalobrian253@gmail.com>
2023-03-17 13:27:10 +0000

    puts

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,9 +9,11 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
-	n = rand() - RAND MAX / 2;
-	if (n >0)
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
@@ -21,7 +23,8 @@ int main(void)
 	}
 	else
 	{
-		printf(%d is negative\n", n);
+		printf("%d is negative\n", n);
 	}
+
 	return (0);
 }
