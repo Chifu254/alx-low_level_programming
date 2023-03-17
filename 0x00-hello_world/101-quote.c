8f79d086b9726314e5ccba004a4947b455daba5d
Chifu254 <kyalobrian253@gmail.com>
2023-03-17 08:38:14 +0000

    news

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,6 @@
 
 /**
  * main - Entry point
- *
  * Return: Always 1 (Error)
  */
 int main(void)
@@ -12,5 +11,6 @@ n = write(STDERR_FILENO,
 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 59);
 if (n == -1)
+return (1);
 return (1);
 }
