cde6c28e005cd509d3c47465849b716ee264d059
Chifu254 <kyalobrian253@gmail.com>
2023-03-30 19:42:13 +0000

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,18 +11,11 @@
  */
 char *_strncat(char *dest, char *src, int n);
 {
-	int index, count;
+	int index = 0, dest_len = 0;
 
-	for (index = 0; dest[index] != '\0'; index++)
-
-	for (count = 0; count < n && src[count] != '\0'; count++)
-	{
-		dest[index] = src[count];
-		index++;
-	}
-	if (count < n)
-	{
-	dest[index] = '\0';
-	}
-	return (dest);
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[dest_len++] = src[index];
+return (dest);
 }
